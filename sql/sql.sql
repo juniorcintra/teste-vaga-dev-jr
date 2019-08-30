54433427da874f9aadc19dd384d7f0743bf6e88d
Junior Cintra <apspvicntraj@gmail.com>
2019-08-30 00:37:59 -0300

    atualizando banco de dados

--- sql/sql.sql
+++ sql/sql.sql
@@ -10,3 +10,20 @@
 INSERT INTO users(name, email, password) VALUES
 ('Junior Cintra', 'apspvcintraj@gmail.com', '2d29b962490320f821db80cddf6ed4b6e4ac7498');
 /*hash SHA-1 de um hash MD5 da string “123mudar”*/
+
+
+CREATE TABLE candidatos(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    email VARCHAR(80) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+    celular VARCHAR(20) NOT NULL,
+    dtNascimento DATE,
+    faculdade TINYINT(11),
+    pretSelario VARCHAR(30),
+    resumoHab TEXT,
+    ativo TINYINT(11) DEFAULT 1,
+    lixeira TINYINT(11) DEFAULT 0,
+    destaque TINYINT(11) DEFAULT 0,
+    PRIMARY KEY(id)
+) COLLATE=utf8_general_ci;
