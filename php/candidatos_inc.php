d196fa159d13ab8115adbb91df3fa849bedeff50
Junior Cintra <apspvicntraj@gmail.com>
2019-08-30 03:10:17 -0300

    implementando insert candidato

--- php/candidatos_inc.php
+++ php/candidatos_inc.php
@@ -1,5 +1,5 @@
 <?php
- 
+
 require 'functions.php';
 
 $nome = isset($_POST['nome']) ? $_POST['nome'] : ''; 
@@ -9,26 +9,27 @@ $celular = isset($_POST['celular']) ? $_POST['celular'] : '';
 $data = isset($_POST['data']) ? $_POST['data'] : '';
 $salario = isset($_POST['salario']) ? $_POST['salario'] : '';
 $resumo = isset($_POST['resumo']) ? $_POST['resumo'] : '';
- 
-if (empty($email) || empty($senha))
-{
-    echo "Informe email e senha";
-    exit;
-}
- 
+$faculdade = isset($_POST['faculdade']) ? 1 : 0;
+$ativo = isset($_POST['ativo']) ? 1 : 0;
+$destaque = isset($_POST['destaque']) ? 1 : 0;
+
+
 
 $PDO = db_connect();
- 
-try {
+$stmt = $PDO->prepare('INSERT INTO candidatos (nome, email, telefone, celular, dtNascimento, faculdade, pretSalario, resumoHab, ativo, destaque, dtCadastro) VALUES(:nome, :email, :telefone, :celular, :dtNascimento, :faculdade, :pretSalario, :resumoHab, :ativo, :destaque, CURRENT_TIMESTAMP)');
+$stmt->execute(array(
+	':nome' => $nome,
+	':email' => $email,
+	':telefone' => $telefone,
+	':celular' => $celular,
+	':dtNascimento' => $data,
+	':faculdade' => $faculdade,
+	':pretSalario' => $salario,
+	':resumoHab' => $resumo,
+	':ativo' => $ativo,
+	':destaque' => $destaque
+));
+echo "<script>alert('Cadastro realizado com sucesso!');</script>";
 
-  $stmt = $PDO->prepare('INSERT INTO candidatos (nome, ) VALUES(:nome)');
-  $stmt->execute(array(
-    ':nome' => 'Ricardo Arrigoni'
-  ));
-   
-} catch(PDOException $e) {
-  echo 'Erro: ' . $e->getMessage();
-  
-}
 
 ?>
